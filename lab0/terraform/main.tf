e1c9c217e43ccd8922fa173c3f050b6f48ad0cba
Mike Truty <miketruty@gmail.com>
2018-07-26 23:17:20 -0700

    Initial simple deployment.

---
+++ lab0/terraform/main.tf
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Terraform main configuration file (with provider definitions).
+ */
+
+provider "google" {
+  version = "~> 1.16.0"
+
+  project = "[YOUR_PROJECT_ID]"
+}
+
+/*
+ * Terraform compute resources for GCP.
+ */
+
+resource "google_compute_instance" "gcp-vm" {
+  name         = "gcp-vm-us-central1"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/family/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Ephemeral IP
+    }
+  }
+}
+
+/*
+ * Terraform output variables for GCP.
+ */
+
+output "external_ip" {
+  value = "${google_compute_instance.gcp-vm.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+
+output "internal_ip" {
+  value = "${google_compute_instance.gcp-vm.network_interface.0.address}"
+}
